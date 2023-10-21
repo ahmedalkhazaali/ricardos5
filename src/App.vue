a580ff744e5dfc7696990cd14aa99eff93869751
yabouelwafa <Yabouelwafa4@gmail.com>
2023-10-21 12:37:19 -0400

    final app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
   <v-app>
     <v-app-bar style="height: 120px" class="pt-7">
-      <v-img src="@/assets/title.png" />
-      <v-btn class="mr-6" to="home">Home</v-btn>
-      <v-btn class="mr-6" to="about">About</v-btn>
-      <v-btn class="mr-6" to="contact">Contact Us</v-btn>
+      <v-img src="@/assets/title.png" class="ml-8" />
+      <v-btn
+        class="mr-6 ml-4"
+        to="home"
+        height="70px"
+        width="150px"
+        style="font-size: 22px"
+        color="green"
+        >Home</v-btn
+      >
+      <v-btn
+        class="mr-6"
+        to="about"
+        height="70px"
+        width="150px"
+        style="font-size: 22px"
+        color="green"
+        :ripple="false"
+        >About</v-btn
+      >
+      <v-btn
+        class="mr-6"
+        to="contact"
+        height="70px"
+        width="200px"
+        color="green"
+        style="font-size: 22px"
+        >Contact Us</v-btn
+      >
       <v-spacer></v-spacer>
       <v-spacer></v-spacer>
       <v-spacer></v-spacer>
-      <div class="mr-16" style="height: 100%">
+      <div class="mr-16 mt-3" style="height: 100%">
+        <v-icon icon="mdi-phone" style="font-size: 50px" class="mr-5 mb-4" />
+        <v-txt class="mr-9 pt-6" style="font-size: 30px"
+          >613 - 233 - 1515</v-txt
+        >
         <v-btn
           width="150px"
           height="60px"
+          class="mb-3"
           style="
             background-color: rgb(238, 155, 0);
             color: white;
-            text-size-adjust: 50px;
+            font-size: 22px;
           "
           to="menu"
           >Menu</v-btn
