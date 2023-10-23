12a37fed430c9edabd20207c5194b5a10547f174
ahmedalkhazaali <ahmed.khazaali2@gmail.com>
2023-10-23 16:40:52 -0400

    abouthome

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,76 +1,98 @@
 <template>
-  <img
-    src="https://pizzapanorama.ca/images/adobestock_266429012.jpg?crc=125304778"
-  />
-  <div class="first">
-    <h1 class="title"><br /><br />RICARDOS PIZZA IS THE BEST IN TOWN</h1>
-    <p>
-      365 days a year, Ricardo's Pizza Restaurant shines with unwavering
-      commitment. We are dedicated to excellence, sourcing only the
-      highest-quality ingredients to create an exceptional pizza experience. Our
-      customers enthusiastically agree: we serve the city's most outstanding
-      pizza. <br /><br /><br /><br />
-    </p>
-  </div>
-  <div class="second">
-    <h1 class="pride"><br />WE PRIDE OURSELVES ON QUALITY <br />AND TASTE</h1>
-    <p class="values">
-      Ricardo's Pizza Restaurant lives by three core values: Craftsmanship,
-      Hospitality, and Sustainability. We're dedicated to crafting exceptional
-      pizzas, offering outstanding service, and practicing sustainability. Your
-      satisfaction is our top priority, and we eagerly anticipate serving you
-      while upholding these values.
-    </p>
-    <v-btn
-      variant="outlined"
-      style="
-        margin-left: 500px;
-        margin-top: 20px;
-        font-size: 20px;
-        background-color: orangered;
-        color: white;
-        border-radius: 3px;
-        border-color: rgb(0, 0, 0);
-      "
-      >View Menu</v-btn
-    >
-    <v-btn
-      variant="outlined"
-      style="
-        font-size: 20px;
-        background-color: rgb(255, 255, 255);
-        color: rgb(0, 0, 0);
-        border-radius: 3px;
-        border-color: rgb(0, 0, 0);
-        margin-left: 5px;
-        margin-top: 20px;
-      "
-      >Contact Us</v-btn
-    >
-    <v-card
-      title="RICARDOS"
-      style="background-color:rgb(138, 138, 138); block-size: 220px;
-   background-image opacity:0.5; background-image: url(https://ca.ooni.com/cdn/shop/articles/20220211142645-margherita-9920.jpg?crop=center&height=800&v=1661341987&width=800) ;background-color: rgb(240, 240, 240); font-size: 50px; color: rgb(0, 0, 0); border-radius: 3px; border-color: rgb(0, 0, 0); margin-right:200px; margin-left: 1200px; margin-top: -300px;"
-      text="WE ARE LOOKING FORWARD TO SERVING YOU."
-      variant="outlined"
-    >
-      <v-card-actions>
-        <v-btn
-          variant="outlined"
-          style="
-            margin-top: 70px;
-            background-color: rgb(138, 138, 138);
-            color: white;
-            border-radius: 3px;
-            border-color: rgb(139, 139, 139);
-          "
-          >Locations</v-btn
-        >
-      </v-card-actions>
-    </v-card>
+  <meta />
+  <div class="container">
+    <img
+      src="https://pizzapanorama.ca/images/adobestock_266429012.jpg?crc=125304778"
+    />
+    <div class="first">
+      <h1 class="title"><br /><br />RICARDOS PIZZA</h1>
+      <h1 class="ttitle">IS THE BEST IN TOWN</h1>
+      <p>
+        365 days a year, Ricardo's Pizza Restaurant shines with unwavering
+        commitment. We are dedicated to excellence, sourcing only the
+        highest-quality ingredients to create an exceptional pizza experience.
+        Our customers enthusiastically agree: we serve the city's most
+        outstanding pizza.<br /><br />
+        <br />
+      </p>
+    </div>
+    <div class="second">
+      <h1 class="pride"><br />WE PRIDE OURSELVES ON QUALITY <br />AND TASTE</h1>
+      <p class="values">
+        Ricardo's Pizza Restaurant lives by three core values: Craftsmanship,
+        Hospitality, and Sustainability. We're dedicated to crafting exceptional
+        pizzas, offering outstanding service, and practicing sustainability.
+        Your satisfaction is our top priority, and we eagerly anticipate serving
+        you while upholding these values.
+      </p>
+      <v-btn
+        variant="outlined"
+        style="
+          margin-left: 26.041%;
+          margin-top: 20px;
+          font-size: 20px;
+          background-color: orangered;
+          color: white;
+          border-radius: 3px;
+          border-color: rgb(0, 0, 0);
+        "
+        >View Menu</v-btn
+      >
+      <v-btn
+        variant="outlined"
+        style="
+          font-size: 20px;
+          background-color: rgb(255, 255, 255);
+          color: rgb(0, 0, 0);
+          border-radius: 3px;
+          border-color: rgb(0, 0, 0);
+          margin-left: 5px;
+          margin-top: 20px;
+        "
+        >Contact Us</v-btn
+      >
+      <v-card
+        title="RICARDOS"
+        style="
+          background-color: rgb(255, 255, 255);
+          block-size: 450px;
+          background-color: rgb(255, 255, 255);
+          font-size: 50px;
+          color: rgb(0, 0, 0);
+          border-radius: 3px;
+          border-color: rgb(0, 0, 0);
+          margin-right: 380px;
+          margin-left: 1070px;
+          margin-top: -350px;
+        "
+        text="WE ARE LOOKING FORWARD TO SERVING YOU."
+        variant="outlined"
+      >
+        <v-card-actions>
+          <v-btn
+            variant="outlined"
+            style="
+              margin-left: 9px;
+              margin-top: -350px;
+              background-color: rgb(255, 255, 255);
+              color: rgb(0, 0, 0);
+              border-radius: 3px;
+              border-color: rgb(0, 0, 0);
+              font-size: 17px;
+            "
+            >Locations</v-btn
+          >
+          <img
+            class="menu"
+            src="https://static.vecteezy.com/system/resources/previews/024/725/117/original/pizza-pizza-pizza-with-transparent-background-ai-generated-free-png.png"
+          />
+        </v-card-actions>
+      </v-card>
 
-    <br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br />
+    </div>
   </div>
+  <div class="logo"></div>
 </template>
 
 <style>
@@ -78,16 +100,29 @@ img {
   width: 100%;
   height: 30%;
 }
+
 .title {
   margin-top: 0px;
   margin-left: 500px;
   margin-right: 500px;
   text-align: center;
-  color: rgb(72, 255, 0);
+  color: #009b55;
   background-color: 218, 218, 218;
-  text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000,
-    2px 2px 0 #000;
-  font-size: 50px;
+  text-shadow: -2px -2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000,
+    2px 2px 0 #000000;
+  font-size: 40px;
+  font-family: "Monospace", Monaco;
+}
+.ttitle {
+  margin-top: 0px;
+  margin-left: 500px;
+  margin-right: 500px;
+  text-align: center;
+  color: #ffffff;
+  background-color: 218, 218, 218;
+  text-shadow: -1px -1px 0 #000000, 1px -1px 0 #000000, -1px 1px 0 #000000,
+    1px 1px 0 #000000;
+  font-size: 40px;
   font-family: "Monospace", Monaco;
 }
 p {
@@ -95,18 +130,28 @@ p {
   margin-right: 500px;
   margin-top: 20px;
   text-align: justify;
-  color: rgb(0, 0, 0);
+  color: rgb(255, 255, 255);
   font-size: 28px;
   font-family: "Monospace", Monaco;
+  text-shadow: -1px -1px 0 #000, 0 -1px 0 #000, 1px -1px 0 #000, 1px 0 0 #000,
+    1px 1px 0 #000, 0 1px 0 #000, -1px 1px 0 #000, -1px 0 0 #000;
 }
+
 .first {
-  background-color: rgb(243, 242, 242);
+  background-color: rgb(253, 134, 65);
   margin-top: -100px;
 }
+
 .values {
   margin-right: 950px;
   font-size: 18px;
+  margin-top: 2px;
+  color: #000;
+  text-shadow: -1px -1px 0 #ffffff, 0 -1px 0 #ffffff, 1px -1px 0 #ffffff,
+    1px 0 0 #ffffff, 1px 1px 0 #ffffff, 0 1px 0 #ffffff, -1px 1px 0 #ffffff,
+    -1px 0 0 #ffffff;
 }
+
 .pride {
   margin-top: 0px;
   margin-left: 495px;
@@ -119,12 +164,35 @@ p {
 }
 
 .second {
-  background-color: rgb(252, 252, 252);
-  background-size: 1000px;
+  background: conic-gradient(
+      from 90deg at 1px 0.5px,
+      #75474700 90deg,
+      rgb(109, 109, 109) 0
+    )
+    0 0/100px 100px;
+  background-color: rgb(255, 255, 255);
+}
+.menu {
+  width: 100%;
+  height: 100%;
+  margin-top: -50px;
+}
+.chicken {
+  width: 100%;
+  height: 100%;
+  margin-top: -50px;
+  margin-right: 1000px;
+}
+.logo {
+  position: absolute;
+  top: 700px;
+  right: 1420px;
+  width: 360px;
+  height: 700px;
+  background-image: url(https://cdn.discordapp.com/attachments/814293260980846612/1165887859287412766/riclogo.png?ex=65487cae&is=653607ae&hm=26b5305888bb1401f95a3ddc94ed040f70d3af0eaa75509f8f99e5b3736af987&);
 }
 </style>
 
 <script setup>
-//
 //
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,221 @@
 <template>
-  <v-btn>Home</v-btn>
+  <div class="ccontainer">
+    <div class="container">
+      <div class="first">
+        <h1 class="title">Ricardos</h1>
+        <h2 class="desc">Ottawa's Number 1 Local <br />Pizza Shop <br /></h2>
+        <h2 class="joy">
+          <br />Pizza that Brings Joy to You and <br />Those You Love
+        </h2>
+        <h2 class="reviews">
+          <br />With over 80 five star
+          <a
+            href="https://www.google.com/search?q=riccardos+pizza+ottawa&sca_esv=575677953&rlz=1C1CHBF_enCA959CA959&biw=1920&bih=991&sxsrf=AM9HkKmpfcAXylyD9h_emIsastJB4I4QWw%3A1698029919651&ei=X-E1ZfysJ_Oj5NoPmLmpoAU&ved=0ahUKEwi82LaxlouCAxXzEVkFHZhcClQQ4dUDCBA&uact=5&oq=riccardos+pizza+ottawa&gs_lp=Egxnd3Mtd2l6LXNlcnAiFnJpY2NhcmRvcyBwaXp6YSBvdHRhd2EyBxAjGLACGCcyDRAuGA0YgAQYxwEYrwEyBxAAGA0YgAQyAhAmMhwQLhgNGIAEGMcBGK8BGJcFGNwEGN4EGOAE2AEDSKY3UABY9jBwAXgAkAEAmAGrAaAB4BCqAQQxNC44uAEDyAEA-AEBqAIRwgIHECMY6gIYJ8ICEBAAGIoFGOoCGLQCGEPYAQHCAhkQLhiKBRjHARjRAxjIAxjqAhi0AhhD2AECwgIEECMYJ8ICBxAjGIoFGCfCAgcQABiKBRhDwgIKEAAYigUYsQMYQ8ICCxAuGIAEGMcBGNEDwgIOEC4YigUYxwEYrwEYkQLCAg0QLhiKBRjHARivARhDwgINEC4YigUYxwEY0QMYQ8ICERAuGIAEGLEDGIMBGMcBGK8BwgIFEAAYgATCAgUQLhiABMICCBAAGIoFGJECwgIUEC4YigUYsQMYgwEYxwEY0QMYkQLCAgsQLhiDARixAxiABMICDRAuGK8BGMcBGIoFGEPCAg0QLhiKBRixAxiDARhDwgIKEAAYgAQYFBiHAsICDRAuGIAEGLEDGIMBGArCAgcQLhiABBgKwgIHEAAYgAQYCsICDRAuGK8BGMcBGIAEGArCAgsQLhiABBjHARivAcICChAAGIAEGLEDGArCAg0QLhiABBjHARivARgKwgIGEAAYFhgewgIIEAAYFhgeGArCAggQABiKBRiGA-IDBBgAIEGIBgG6BgYIARABGAG6BgYIAhABGAi6BgYIAxABGBQ&sclient=gws-wiz-serp#"
+            target="_blank"
+            style="font-size: 22px; color: white"
+            >Reviews</a
+          >
+          <br />
+          <br />
+        </h2>
+        <v-btn
+          variant="outlined"
+          size="large"
+          style="
+            font-size: 20px;
+            background-color: rgb(255, 255, 255);
+            color: rgb(0, 0, 0);
+            border-radius: 3px;
+            border-color: rgb(0, 0, 0);
+            margin-left: 5px;
+            margin-top: 20px;
+          "
+        >
+          Order Now
+        </v-btn>
+      </div>
+      <div>
+        <h1 class="border">LOCALLY OWNED PIZZA THAT BRINGS JOY</h1>
+      </div>
+    </div>
+  </div>
+  <div class="backround"></div>
+  <div class="logo"></div>
+  <div class="us">
+    <h1 class="our"><br /><br /><br /><br />Our pizzas<br /><br /></h1>
+
+    <h1 class="p">
+      Ricardo's Pizza, a family-owned establishment in ottawa, has been serving
+      top-quality food since 2003. Our dedication to excellence means using only
+      the freshest ingredients in our kitchen. From mouthwatering pizza to
+      delectable salads, wings, and more, our diverse menu goes beyond just
+      pizza. Come and experience the delicious difference at Ricardo's Pizza
+    </h1>
+  </div>
+  <v-btn
+    variant="outlined"
+    style="
+      font-size: 20px;
+      background-color: transparent;
+      color: rgb(255, 255, 255);
+      border-radius: 3px;
+      border-color: rgb(255, 255, 255);
+      margin-right: 970px;
+      margin-left: 635px;
+      margin-top: 670px;
+    "
+  >
+    About Us
+  </v-btn>
+  <v-card
+    style="
+      background-color: transparent;
+      block-size: 350px;
+      background-color: transparent;
+      font-size: 50px;
+      color: rgb(0, 0, 0);
+      border-radius: 3px;
+      border-color: transparent;
+      margin-right: 400px;
+      margin-left: 1070px;
+      margin-top: -400px;
+    "
+    variant="outlined"
+  >
+    <v-card-actions>
+      <img
+        class="menu"
+        src="https://cdn.loveandlemons.com/wp-content/uploads/2023/02/vegetarian-pizza-500x500.jpg"
+      />
+    </v-card-actions>
+  </v-card>
 </template>
+
+<style>
+.menu {
+  width: 100%;
+  height: 30%;
+  border-radius: 10px;
+  border-color: #ffffff;
+  border: px solid rgb(255, 255, 255);
+  filter: drop-shadow(rgb(0, 0, 0) 1rem 1rem 10px);
+}
+.first {
+  margin-left: 396px;
+  text-align: justify;
+  color: rgb(255, 255, 255);
+  font-family: "Monospace", Monaco;
+  color: white;
+  text-shadow: -1px -1px 0 #000, 0 -1px 0 #000, 1px -1px 0 #000, 1px 0 0 #000,
+    1px 1px 0 #000, 0 1px 0 #000, -1px 1px 0 #000, -1px 0 0 #000;
+}
+.title {
+  color: #009b55;
+  background-color: 218, 218, 218;
+  text-shadow: -3px -3px 0 #000, 3px -3px 0 #000, -3px 3px 0 #000,
+    3px 3px 0 #000;
+  font-size: 120px;
+  font-family: "Monospace", Monaco;
+}
+.desc {
+  color: rgb(255, 255, 255);
+  background-color: 218, 218, 218;
+  text-shadow: -3px -3px 0 #000, 3px -3px 0 #000, -3px 3px 0 #000,
+    3px 3px 0 #000;
+  font-size: 55px;
+  font-family: "Monospace", Monaco;
+}
+.joy {
+  color: rgb(255, 255, 255);
+  background-color: 218, 218, 218;
+  font-size: 35px;
+  text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000,
+    2px 2px 0 #000;
+  font-family: "Monospace", Monaco;
+  margin-top: -40px;
+}
+.reviews {
+  color: rgb(255, 255, 255);
+  background-color: 218, 218, 218;
+  font-size: 25px;
+  font-family: "Monospace", Monaco;
+  margin-top: -40px;
+}
+.container {
+  min-height: 700px;
+  max-height: 800px;
+  margin-right: 870px;
+  background-color: rgb(253, 134, 65);
+}
+.border {
+  min-width: 1920px;
+  margin-top: 200px;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  background-color: #ffffff;
+  text-shadow: -1px -1px 0 #000, 0 -1px 0 #000, 1px -1px 0 #000, 1px 0 0 #000,
+    1px 1px 0 #000, 0 1px 0 #000, -1px 1px 0 #000, -1px 0 0 #000;
+  background: conic-gradient(
+      from 90deg at 1px 0.5px,
+      #75474700 90deg,
+      rgb(109, 109, 109) 0
+    )
+    0 0/100px 100px;
+}
+.backround {
+  position: absolute;
+  top: 0px;
+  right: 0;
+  width: 865px;
+  height: 799px;
+  border: 0px solid rgb(255, 159, 103);
+  background-image: url(https://pizzatomorrow.com/wp-content/uploads/2021/11/HeroBackground_PT.jpg);
+}
+.logo {
+  position: absolute;
+  top: 30px;
+  right: 1520px;
+  width: 360px;
+  height: 700px;
+  background-image: url(https://cdn.discordapp.com/attachments/814293260980846612/1165887859287412766/riclogo.png?ex=65487cae&is=653607ae&hm=26b5305888bb1401f95a3ddc94ed040f70d3af0eaa75509f8f99e5b3736af987&);
+}
+.us {
+  text-align: justify;
+  position: absolute;
+  top: 848px;
+  right: 0;
+  width: 1920px;
+  height: 800px;
+  background-image: url(https://static.vecteezy.com/system/resources/previews/025/741/273/original/grunge-texture-effect-distressed-overlay-rough-textured-on-dark-space-realistic-gray-background-graphic-design-element-concrete-wall-style-concept-for-banner-flyer-poster-brochure-cover-etc-vector.jpg);
+  text-shadow: -1px -1px 0 #000, 0 -1px 0 #000, 1px -1px 0 #000, 1px 0 0 #000,
+    1px 1px 0 #000, 0 1px 0 #000, -1px 1px 0 #000, -1px 0 0 #000;
+}
+.our {
+  margin-right: 970px;
+  margin-left: 635px;
+  color: #ffffff;
+  background-color: 218, 218, 218;
+  font-size: 40px;
+  font-family: "Monospace", Monaco;
+  text-align: justify;
+}
+.ing {
+  text-align: justify;
+  margin-right: 870px;
+  margin-left: 576px;
+  color: rgb(255, 255, 255);
+  background-color: 218, 218, 218;
+  font-size: 25px;
+  font-family: "Monospace", Monaco;
+}
+.p {
+  text-align: justify;
+  margin-right: 970px;
+  margin-left: 496px;
+  color: rgb(255, 255, 255);
+  background-color: 218, 218, 218;
+  font-size: 20px;
+  font-family: "Monospace", Monaco;
+  margin-top: -40px;
+}
+</style>
